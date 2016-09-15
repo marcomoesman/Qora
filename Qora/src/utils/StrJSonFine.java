a0c3190c0b52cc765c285ecff6c65e4eec551fbe
Skerberus <Buddelb@googlemail.com>
2016-09-15 21:52:04 +0200

    fixed bug when changing access permission -> settings.json got corrupted

--- Qora/src/utils/StrJSonFine.java
+++ Qora/src/utils/StrJSonFine.java
@@ -1,15 +1,17 @@
 package utils;
 
-import java.io.StringWriter;
 import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
+import com.cedarsoftware.util.io.JsonWriter;
+
 public class StrJSonFine{
 
     public static String convert(JSONObject json) {
+    	
     	return convert(json.toJSONString());
     }
     
@@ -23,60 +25,7 @@ public class StrJSonFine{
     }
     
     public static String convert(String str) {
-        int indent = 0;
-        
-        boolean value = false;
-        
-    	char[] chars = str.toCharArray();
-    	StringWriter output = new StringWriter();
-    	
-    	char ch;
-    	char prevCh = 0;
-    	
-    	for (int n = 0; n < chars.length; n++) {
-    		ch = chars[n];
-    		
-    		if(ch == '"' && prevCh != '\\' )
-    		{
-    			value = !value;
-    		}
-
-    		if(!value)
-    		{
-	    		if (ch == '[' || ch == '{') {
-	    			output.write(ch);
-	    			output.write("\r\n");
-	    		    indent++;
-	    		    writeIndentation(output, indent);
-	    		} else if (ch == ',') {
-	    			output.write(ch);
-	    			output.write("\r\n");
-	    			writeIndentation(output, indent);
-	    		} else if (ch == ']' || ch == '}') {
-	    			output.write("\r\n");
-	    		    indent--;
-	    		    writeIndentation(output, indent);
-	    		    output.write(ch);
-	    		} else if (ch == ':') {
-	    			output.write(ch);
-	    			output.write(" ");
-    			}
-    			else 
-    			{
-	    			output.write(ch);
-	    		}
-    		}
-    		else {
-    			output.write(ch);
-	    	}
-    		prevCh = ch;
-		}
-    	return output.toString();
+    	return  JsonWriter.formatJson(str);
     }
     
-	private static void writeIndentation(StringWriter writer, int indent) {
-		for (int i = 0; i < indent; i++) {
-			writer.write('\t');
-		}
-    }
 }
